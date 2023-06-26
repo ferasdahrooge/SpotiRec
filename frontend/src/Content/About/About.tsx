3a9975d574331323548f62cf3aeecd261e18ccf7
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-06-26 17:12:09 -0600

    Add About me

--- frontend/src/Content/About/About.tsx
+++ frontend/src/Content/About/About.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./About.scss";
 
 const About: React.FC = () => {
-  return <div className='About'>about</div>;
+  return (
+    <div className='About'>
+      <h1> About </h1>
+      <br></br>
+      <p>
+        A recommendation system that is designed to cater to your unique
+        personality and preferences. Feel free to talk about anything.
+        <br />
+        As our system analyzes your inputs to deliver the best music
+        recommendations. Whether it's your mood, favorite genres, or specific
+        artists, everything is taken into account to provide you with a
+        personalized and enjoyable music experience
+      </p>
+    </div>
+  );
 };
 
 export default About;

c6362c630dbc4e4b1d53bc8ac8f4ea4f6c2c6d03
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-06-28 10:02:18 -0600

    apply mobile active, prefixed

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 interface HeaderProps {
   title: Record<string, string>;
@@ -8,6 +9,10 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ title, links }) => {
+  const [click, setclick] = useState(false);
+
+  const handleClick = () => setclick(!click);
+
   return (
     <div className='Header'>
       <div className='Header__title'>
@@ -19,17 +24,27 @@ const Header: React.FC<HeaderProps> = ({ title, links }) => {
           );
         })}
       </div>
-      <ul className='Header__links'>
-        {Object.keys(links).map((link) => {
-          return (
-            <li key={link}>
-              <NavLink className='Header__link' to={link}>
-                {links[link]}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
+      <div onClick={handleClick} className='Header__icons'>
+        {click ? <FaTimes /> : <FaBars />}
+      </div>
+      <div className={`Header__slide ${click ? "active" : ""}`}>
+        <ul className={click ? "Header__links active" : "Header__links"}>
+          {Object.keys(links).map((link) => {
+            return (
+              <li key={link}>
+                <NavLink
+                  className={({ isActive }) =>
+                    "Header__link" + (isActive ? " activated" : "")
+                  }
+                  to={link}
+                >
+                  {links[link]}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };

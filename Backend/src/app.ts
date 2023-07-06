1a9ad81def9d19141e95c5341fcf1a533f55fb18
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-07-06 13:35:38 -0600

    test post

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 const PORT = 8080;
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/data", (req, res) => {
+  console.log(req.body);
+  return res.sendStatus(200);
 });
 
 app.listen(8080, () => {

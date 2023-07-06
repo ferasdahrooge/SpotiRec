adf6572828ba5bf29efef80997da7166ed342f86
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-07-06 15:08:29 -0600

    connect frontend  backend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,25 +2,17 @@ import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.use(express.json());
-
-function handleGetRequest(req: Request, res: Response, next: NextFunction) {
-  console.log(req.params);
-  next();
-}
-
-function handleGetValue(req: Request, res: Response, next: NextFunction) {
-  console.log(req.params.textId);
-  return res.sendStatus(200);
-}
-
-app.get("/api/data/:textId", [handleGetRequest as any, handleGetValue]);
-
-app.route(/abc/).post((req: Request, res: Response) => {
-  return res.send("POST ");
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("Server is listening on port 8080");
 });

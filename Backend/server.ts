3f25ce462809737c9d871459b8e8ecaf4f456e08
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-07-06 13:27:43 -0600

    remove server

--- Backend/server.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response } from "express";
-const PORT = 8000;
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
-
-app.listen(PORT, () => console.log("Server Running"));

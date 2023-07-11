ede61fae268979dbb83aed989dc36204e2379637
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-07-10 22:41:22 -0600

    POST text

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,18 +1,25 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import { Server } from "http";
+import bodyParser from "body-parser";
 
-const app = express();
+const app: Application = express();
 const PORT = 8080;
 const cors = require("cors");
 app.use(
   cors({
     origin: "http://localhost:3000",
-  })
+  }),
+  bodyParser.json()
 );
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World");
+app.post("/text/", (req: Request, res: Response, next: NextFunction) => {
+  const { data } = req.body;
+  return res.status(200).json({
+    message: "Success",
+    data: data,
+  });
 });
 
-app.listen(PORT, () => {
+const server: Server = app.listen(PORT, () => {
   console.log("Server is listening on port 8080");
 });
